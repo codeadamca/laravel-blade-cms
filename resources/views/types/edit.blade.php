db6a023dd21195df63bd51ca9a9e6b9f1ca4a8f2
Adam Thomas <thomasadam83@hotmail.com>
2022-04-03 23:03:05 -0400

    Converted types to blade

--- resources/views/types/edit.blade.php
+++ resources/views/types/edit.blade.php
@@ -1,62 +1,31 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends ('layout.console')
 
-        <title>My Portfolio</title>
+@section ('content')
 
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-        <link rel="stylesheet" href="/app.css">
+<section class="w3-padding">
 
-        <script src="/app.js"></script>
-        
-    </head>
-    <body>
+    <h2>Edit Type</h2>
 
+    <form method="post" action="/console/types/edit/{{$type->id}}" novalidate class="w3-margin-bottom">
 
-        <header class="w3-padding">
+        @csrf
 
-            <h1 class="w3-text-red">Portfolio Console</h1>
+        <div class="w3-margin-bottom">
+            <label for="title">Title:</label>
+            <input type="text" name="title" id="title" value="{{old('title', $type->title)}}" required>
+            
+            @if ($errors->first('title'))
+                <br>
+                <span class="w3-text-red">{{$errors->first('title')}}</span>
+            @endif
+        </div>
 
-            <?php if(Auth::check()): ?>
-                You are logged in as <?= auth()->user()->first ?> <?= auth()->user()->last ?> | 
-                <a href="/console/logout">Log Out</a> | 
-                <a href="/console/dashboard">Dashboard</a> | 
-                <a href="/">Website Home Page</a>
-            <?php else: ?>
-                <a href="/">Return to My Portfolio</a>
-            <?php endif; ?>
+        <button type="submit" class="w3-button w3-green">Edit Type</button>
 
-        </header>
+    </form>
 
-        <hr>
+    <a href="/console/types/list">Back to Type List</a>
 
-        <section class="w3-padding">
+</section>
 
-            <h2>Edit Type</h2>
-
-            <form method="post" action="/console/types/edit/<?= $type->id ?>" novalidate class="w3-margin-bottom">
-
-                <?= csrf_field() ?>
-
-                <div class="w3-margin-bottom">
-                    <label for="title">Title:</label>
-                    <input type="text" name="title" id="title" value="<?= old('title', $type->title) ?>" required>
-                    
-                    <?php if($errors->first('title')): ?>
-                        <br>
-                        <span class="w3-text-red"><?= $errors->first('title'); ?></span>
-                    <?php endif; ?>
-                </div>
-
-                <button type="submit" class="w3-button w3-green">Edit Type</button>
-
-            </form>
-
-            <a href="/console/types/list">Back to Type List</a>
-
-        </section>
-
-    </body>
-</html>
+@endsection
